4fed063c64c8a53cf9154fc6bdb8bc3e8bcf3f36
QirongZhang <qrzhang@thoughtworks.com>
2022-06-21 22:23:27 +0800

    remove useless test case for parking.

--- src/test/java/org/oobootcamp/parking/lot/ParkingTest.java
+++ src/test/java/org/oobootcamp/parking/lot/ParkingTest.java
@@ -9,14 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ParkingTest {
-
-    @Test
-    void should_return_specific_parking_when_create_parking_given_specific_parking_lot_number() {
-        Parking parking = new Parking(1);
-
-        assertThat(parking.hasEmptyLot()).isTrue();
-    }
-
     // this test is useless?
     // should we modify test after some feature is done
     @Test
@@ -49,7 +41,6 @@ public class ParkingTest {
     void should_return_ticket_when_parking_a_car_successful() {
         Parking parking = new Parking(1);
         Car car1 = new Car();
-
 
         Ticket ticket = parking.park(car1);
 
