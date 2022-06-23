3165e40031a5f8f8a40abee9f0dbaa96f1055afa
QirongZhang <qrzhang@thoughtworks.com>
2022-06-23 22:17:53 +0800

    add new case for multiple parking lot.

--- src/test/java/org/oobootcamp/parking/lot/SmartParkingBoyTest.java
+++ src/test/java/org/oobootcamp/parking/lot/SmartParkingBoyTest.java
@@ -29,4 +29,17 @@ public class SmartParkingBoyTest {
 
         assertThat(firstParkingLot.hasCar(ticket)).isTrue();
     }
+
+    @Test
+    void should_parking_a_car_to_second_parking_lot_and_get_ticket_when_park_a_car_given_second_parking_lot_with_more_empty_spots() {
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(2);
+
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(List.of(firstParkingLot, secondParkingLot));
+        Car car = new Car();
+
+        Ticket ticket = smartParkingBoy.park(car);
+
+        assertThat(secondParkingLot.hasCar(ticket)).isTrue();
+    }
 }
